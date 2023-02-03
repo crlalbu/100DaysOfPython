f61fdc41c93256ec79658b9661f45f24863240ff
Carlos <crl.albu@gmail.com>
2023-02-02 21:25:33 -0300

    day 9

---
+++ day9/main.py
@@ -0,0 +1,17 @@
+#Nesting
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+#Nesting a List in a Dictionary
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+#Nestinf Dictionary in a Dictionary
+travel_log2 = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 15},
+}
